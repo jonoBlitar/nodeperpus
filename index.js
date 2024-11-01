89423822b29deefef9bcc12c56ba4e386c496410
AKNBlitar <jono@akb.ac.id>
2024-11-01 14:15:29 +0700

    setting untuk vercel

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ const startServer = async () => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.get('/',(req,res)=>{
+res.json({message:"Hello coba backend untuk vercel"});
+});
 app.use('/api/buku',bukuRoute);
 app.use('/api/prodi',prodRoute);
 app.use('/api/mhs',mhsRoute);
