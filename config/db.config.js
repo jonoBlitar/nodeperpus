875d2dda48c2f887a9848865bbb6aead1eb7fc99
jonoBlitar <109195188+jonoBlitar@users.noreply.github.com>
2024-11-07 14:30:54 +0700

    Update db.config.js
    coba hosting mysql

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize('web_lanjut', 'root', '', {
-  host: "localhost",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   
   dialectOptions: {
