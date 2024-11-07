e8cf4b88783fef64e5923117ceca283f675bd1c6
jonoBlitar <109195188+jonoBlitar@users.noreply.github.com>
2024-11-07 14:50:27 +0700

    Update db.config.js
    upload mysql di hosting

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
-  dialectModule: require('mysql2'),
+  dialectModule: import('mysql2'),
   
   
   "define": {
