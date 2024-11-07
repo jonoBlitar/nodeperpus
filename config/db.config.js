346e9a5473c16454aa6ec095ea523d57dd32ce32
jonoBlitar <109195188+jonoBlitar@users.noreply.github.com>
2024-11-07 15:57:59 +0700

    Update db.config.js
    coba mysql update

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize";
+import mysql2 from 'mysql2';
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
