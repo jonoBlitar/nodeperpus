38e03f2686920b046e6b6ab43c38d1b672d1cd5d
jonoBlitar <109195188+jonoBlitar@users.noreply.github.com>
2024-11-07 14:41:03 +0700

    Update db.config.js
    edit untuk mysql agar jalan

--- config/db.config.js
+++ config/db.config.js
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
+  dialectModule: require('mysql2')
   
   dialectOptions: {
     charset: 'utf8mb4',     // Ganti charset menjadi utf8 atau utf8mb4
