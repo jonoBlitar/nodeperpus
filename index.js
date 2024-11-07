58fe36e7105a9f954422bd612b5831567c32bfb3
jonoBlitar <109195188+jonoBlitar@users.noreply.github.com>
2024-11-07 14:29:20 +0700

    Update index.js
    coba pakai hosting mysql

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
-/*import db from "./config/db.config.js";
+import db from "./config/db.config.js";
 import bukuRoute from "./routes/buku.routes.js"
 import prodRoute from"./routes/prodi.routes.js"
 import mhsRoute from "./routes/mahasiswa.routes.js"
 import pinjamRoute from "./routes/pinjam.routes.js"
-import userRoute from "./routes/user.routes.js"*/
+import userRoute from "./routes/user.routes.js"
 import cors from "cors";
 
 const app=express();
 
-/*const startServer = async () => {
+const startServer = async () => {
     try {
       // Menggunakan await untuk sequelize.authenticate di dalam async function
       await db.authenticate();
@@ -22,19 +22,19 @@ const app=express();
       console.error('Failed to connect to the database:', error);
     }
   };
-*/
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get('/',(req,res)=>{
 res.json({message:"Hello coba backend untuk vercel"});
 });
-/*app.use('/api/buku',bukuRoute);
+app.use('/api/buku',bukuRoute);
 app.use('/api/prodi',prodRoute);
 app.use('/api/mhs',mhsRoute);
 app.use('/api/pinjam',pinjamRoute);
 app.use('/api/user',userRoute);
-startServer();*/
+startServer();
 app.listen(5000);
 export default app;
 
